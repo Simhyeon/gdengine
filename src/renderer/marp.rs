f688a7e94122c375c973dcf51b9116115f9cc749
simoncreek <simoncreek@tutanota.com>
2021-08-27 02:26:57 +0900

    Changed format logic

--- src/renderer/marp.rs
+++ src/renderer/marp.rs
@@ -1,23 +1,15 @@
 use std::path::{PathBuf, Path};
 use std::process::Command;
 use crate::error::GdeError;
-use crate::utils;
 
-pub(crate) fn marp_render(file: PathBuf, format: Option<String>, out_name: &Path) -> Result<(), GdeError> {
-    // Set file format
-    let format = if let None = format {
-        "".to_owned()
-    } else {
-        format!("{}",format.unwrap())
-    };
-
+pub(crate) fn marp_render(file: PathBuf, format: &str, out_name: &Path) -> Result<(), GdeError> {
     let output = Command::new("marp")
         .arg(file)
         .arg("--allow-local-files")
         .arg("--html")
         .arg(format!("--{}",format))
         .arg("-o")
-        .arg(format!("{}.{}",utils::cache_path()?.join("out").display(),format))
+        .arg(out_name)
         .output()?;
 
     eprintln!("{}", String::from_utf8_lossy(&output.stderr));
