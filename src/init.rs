01cf2c840e39725baa36df559757de0afd059f61
simoncreek <simoncreek@tutanota.com>
2021-08-27 00:15:20 +0900

    Moved to rad

--- src/init.rs
+++ src/init.rs
@@ -15,23 +15,14 @@ impl Init {
 
     // Create new macro file
     pub fn new_macro_file() -> Result<(), GdeError> {
-        Init::m4_macro_file()?;
+        Init::macro_file()?;
         Ok(())
     }
 
     // Crate new m4 macro file
-    fn m4_macro_file() -> Result<(), GdeError> {
+    fn macro_file() -> Result<(), GdeError> {
         // Index m4 file
-        fs::write(Path::new("index.m4"), "divert(`-1')\n\ndivert`'")?;
-        // env file
-        fs::write(Path::new("env.m4"), "divert(`-1')\n\ndivert`'")?;
-        Ok(())
-    }
-
-    // TODO 
-    // Complete this when rad is complete
-    fn rad_macro_file() -> Result<(), GdeError> {
-        unimplemented!();
+        fs::write(Path::new("index.r4d"), "")?;
         Ok(())
     }
 
