dc336990d6bac133ac21850ef99aeaa6844ee109
simoncreek <simoncreek@tutanota.com>
2021-10-04 00:38:42 +0900

    Welp makefile was not a good idea

---
+++ auto.sh
@@ -0,0 +1,19 @@
+echo "$PWD/renderers/marp/bin/marp"
+
+if [ ! -f "$PWD/renderers/marp/bin/marp" ]; then
+	echo "Marp doesn't exist renderers/marp/bin"
+	exit 1
+fi
+if [ ! -f "$PWD/renderers/marp/bin/chrome/chrome" ]; then
+	echo "Chrome doesn't exist in path renderers/marp/bin"
+	exit 1
+fi
+if [ ! -f "$PWD/renderers/pandoc/bin/pandoc" ]; then
+	echo "Pandoc doesn't exist in path renderers/pandoc/bin"
+	exit 1
+fi
+cargo build --release
+strip ./target/release/gde
+cp ./target/release/gde ./gde
+tar -cvzf ./target/gdengine_x86_64-unknown-linux-gnu.tar.gz gde libs renderers
+rm gde
