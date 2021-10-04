9857bf7ebacb36b448584b3c33acd0d66a8c122f
simoncreek <simoncreek@tutanota.com>
2021-10-05 01:59:05 +0900

    Read env file

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,13 @@ mod renderer;
 mod utils;
 
 use cli::{Cli, Variant};
+extern crate dotenv;
+use dotenv::dotenv;
 
 fn main() {
+    // Use dotenv file's variables
+    dotenv().ok();
+
     let result = Cli::new(Variant::None).parse();
     if let Some(err) = result.err() {
         eprintln!("{}", err);
