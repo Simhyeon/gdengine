a72a596a84086a444574f2ecb1860abf79e0eccd
simoncreek <simoncreek@tutanota.com>
2021-11-02 19:16:45 +0900

    Named script usage

--- src/config.rs
+++ src/config.rs
@@ -27,20 +27,45 @@ impl Config {
     /// Create new config json string
     pub fn new_file() -> String {
         String::from(r#"{
-    "run": [],
-    "test": []
+    "run": {"marp": ["render -m marp"]},
+    "test": {"marp": ["render -m marp --test --preserve"]}
 }"#)
     }
 
-    pub fn get_value_as_array(&self, name:&str) -> Option<Vec<Value>> {
-        if let Some(value) =self.content.get(name) {
-            if let Value::Array(arr) = value {
-                Some(arr.to_owned())
+    pub fn get_run_script(&self, name:Option<&str>) -> Result<Option<Vec<Value>>, GdeError> {
+        self.get_script("run", name)
+    }
+
+    pub fn get_test_script(&self, name:Option<&str>) -> Result<Option<Vec<Value>>, GdeError> {
+        self.get_script("test", name)
+    }
+
+    fn get_script(&self, script_type:&str, name: Option<&str>) -> Result<Option<Vec<Value>>, GdeError> {
+        if let Some(value) =self.content.get(script_type) {
+            let new_name :&str = if let None = name {
+                let vec = value
+                    .as_object()
+                    .unwrap()
+                    .into_iter()
+                    .collect::<Vec<_>>();
+
+                // Empty
+                if vec.len() == 0 {
+                    return Err(GdeError::ConfigError(format!("{} has empty content", script_type)));
+                }
+
+                // else return key of first element
+                vec[0].0
+            } else { name.unwrap() };
+
+            if let Some(Value::Array(arr)) = value.get(new_name) {
+                Ok(Some(arr.clone()))
             } else {
-                None
+                eprintln!("No such target to execute : {}", new_name);
+                Ok(None)
             }
         } else {
-            None
+            Err(GdeError::ConfigError("No run element in config".to_owned()))
         }
     }
 }
