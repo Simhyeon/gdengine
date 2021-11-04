dfe9122c4ce2c0adabfcd5a7b8e70c7dac9b1290
simoncreek <simoncreek@tutanota.com>
2021-11-04 20:58:41 +0900

    Bumped version

--- src/cli.rs
+++ src/cli.rs
@@ -114,7 +114,7 @@ impl Cli {
 
     fn args_builder() -> clap::App<'static> {
         App::new("gde")
-            .version("0.4.0")
+            .version("0.4.1")
             .author("Simon creek <simoncreek@tutanota.com>")
             .about("Gdengine is a document automation program.")
             .setting(clap::AppSettings::ArgRequiredElseHelp)
